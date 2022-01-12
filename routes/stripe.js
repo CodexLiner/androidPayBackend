e2922e745789687a02ab7d962e2656ff6a2af151
Gopal Meena <meenagopal24@gmail.com>
2022-01-12 23:51:11 +0530

    secured

--- routes/stripe.js
+++ routes/stripe.js
@@ -3,9 +3,7 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const router = express.Router();
 //stripe sdk
-const stripe = require("stripe")(
-  "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"
-);
+const stripe = require("stripe")(process.env.STRIPEKEY);
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
